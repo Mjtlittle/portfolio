b1d1948ac7528acaeffbb7d909bb9b915e95525f
Joshua <Jplittle2001@gmail.com>
2020-10-04 18:25:15 -0400

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,14 +4,17 @@ import Head from './Head'
 import Topbar from './Topbar'
 
 import styles from './Layout.module.sass'
+import '../styles/global.sass'
 
 const Layout = ({ title, children }) => {
   return (
     <React.Fragment>
       <Head title={title} />
       <div className={styles.root}>
-        <Topbar />
-        <div className={styles.content}>{children}</div>
+        <div className={styles.content}>
+          <Topbar />
+          {children}
+        </div>
       </div>
     </React.Fragment>
   )
